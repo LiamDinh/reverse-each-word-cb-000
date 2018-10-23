0dd2f05a31f5d5ab83cee87b00092c2f05f20a28
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-10-23 16:53:01 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
 new_sentence = sentence.split
 new_string = ""
 new_sentence.collect do |word|
-  new_string << word.reverse
+  new_string = new_string + " " + word.reverse
 end
 new_string
 end
